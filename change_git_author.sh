e7064ea913ca42bfc917dc23ed6a47f5c6747179
Joseph Ajibodu <josephajibodu@gmail.com>
2024-10-10 20:19:39 +0100

    Update change_git_author.sh

--- change_git_author.sh
+++ change_git_author.sh
@@ -7,11 +7,14 @@
 
    if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
    then
+       echo "Changing committer email for commit $GIT_COMMIT"
        export GIT_COMMITTER_NAME="$CORRECT_NAME"
        export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
    fi
    if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
    then
+       echo "Changing author email for commit $GIT_COMMIT"
        export GIT_AUTHOR_NAME="$CORRECT_NAME"
        export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
    fi
+   ' --tag-name-filter cat -- --branches --tags
